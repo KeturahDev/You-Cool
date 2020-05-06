7472f91b2e9264ffaf9a0f3744caf9a3398de3b3
Keturah Howard <keturah.dev@gmail.com>
2020-05-05 23:11:15 -0700

    create local state with hook to toggle view

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,34 @@ import CoolCard from './CoolCard';
 import Form from './Form';
 
 function App() {
-
+  const [currentPage, setPage] = useState('CoolCard')
+  const handleClick = () => {
+    if(currentPage === 'CoolCard') {
+      setPage('Form')
+    } else if ( currentPage === 'Form') {
+      setPage('CoolCard')
+    }
+  }
+  const setVisibility = () => {
+    if(currentPage === 'CoolCard') {
+      return {
+        component: <CoolCard />,
+        buttonText: "Add new Compiment"
+      }
+    } else if ( currentPage === 'Form') {
+      return {
+        component: <Form />,
+        buttonText: "Back to Compliments"
+      }
+    }
+  }
+  const currentlyVisible = setVisibility()
   return (
     <div className="App">
       <Header />
       {/* dynamically render cool card or button text based on visibility state */}
-      <CoolCard />
-      <Form />
-      <button>button text</button>
+      {currentlyVisible.component}
+      <button onClick={handleClick}>{currentlyVisible.buttonText}</button>
     </div>
   );
 }
